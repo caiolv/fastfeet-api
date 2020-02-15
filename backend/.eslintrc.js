3890d92fe5b0a241ec27dc4c0ce7889ce7f967b4
Caio Oliveira <caiolv@me.com>
2020-02-15 17:26:55 -0300

    Adicionar alterações do ESLint

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -15,5 +15,9 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
   },
 };
