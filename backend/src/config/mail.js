9aee8877841d6e7a31dea6da245d72ed8d20af4f
Caio Oliveira <caiolv@me.com>
2020-03-10 16:23:18 -0300

    Adição de função para enviar e-mails

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '34d389ed5ab83e',
+    pass: 'b41e1a1563f6e7',
+  },
+  default: {
+    from: 'Equipe FastFeet <noreply@fastfeet.com>',
+  },
+};
